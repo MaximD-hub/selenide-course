0b5c8db7aac09f07a6e693a187ab2127805eabd1
Max Dedyura <Maxim.Dedyura@kaspi.kz>
2025-12-09 17:08:17 +0500

    Added new test for GitHub

---
+++ src/test/java/SelenideRepositorySearch.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+// чтобы можо было писать open(); без Selenide.open();
+
+public class SelenideRepositorySearch {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        // открыть главную страницу
+        open("https://github.com/");
+
+        // ввести в поле поиска selenide и нажать enter
+        $("[aria-label='Search or jump to…']").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+
+        // кликнуть на первый репозиторий из списка найденных
+        $$("[data-testid='results-list'] a").first().click();
+        // проверка: заголовок selenide/selenide
+
+    }
+
+}
